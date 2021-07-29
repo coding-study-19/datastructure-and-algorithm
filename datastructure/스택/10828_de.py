018f4b30c298acbc98a13d8a80129e67337c90cf
kimdoeon <ilsin205@naver.com>
2021-07-29 22:44:21 +0900

    Add 9093_de.py

---
+++ datastructure/스택/10828_de.py
@@ -0,0 +1,25 @@
+import sys
+n = int(sys.stdin.readline())
+stack=[]
+
+for i in range(n):
+    command_line = sys.stdin.readline().split()
+    if(command_line[0]=="push"):
+        stack.append(command_line[1])
+    elif(command_line[0]=="pop"):
+        if stack :
+            print(stack.pop())
+        else:
+            print('-1')
+    elif(command_line[0]=="size"):
+        print(len(stack))
+    elif(command_line[0]=="empty"):
+        if stack : 
+            print('0')
+        else : 
+            print('1')
+    elif(command_line[0]=="top"):
+        if stack:
+            print(stack[-1])
+        else:
+            print('-1')
