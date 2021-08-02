bd6112ac858bb6518d7e09770093aacc913212f8
kimdoeon <54973036+kimdoeon@users.noreply.github.com>
2021-08-02 12:15:36 +0900

    Add bj_de

--- datastructure/덱/1158_de.py
+++ datastructure/덱/1158_de.py
@@ -26,3 +26,18 @@ print("<{}>".format(", ".join(list(map(str,result)))))
 
 #join함수는 문자열 리스트만 취급하기 때문에 정수 리스트는 문자열 리스트로 변환해야 함
 # 객체 복사 시 주소가 참조되는 것 =>copy 사용으로 해결
+
+# 리스트 순환은 나머지 연산으로 구현할 수 있다.
+# N,K = map(int,input().split())
+# arr = [i for i in range(1,N+1)]    # 맨 처음에 원에 앉아있는 사람들
+
+# answer = []   # 제거된 사람들을 넣을 배열
+# num = 0  # 제거될 사람의 인덱스 번호
+
+# for t in range(N):
+#     num += K-1  
+#     if num >= len(arr):   # 한바퀴를 돌고 그다음으로 돌아올때를 대비해 값을 나머지로 바꿈  
+#         num = num%len(arr)
+ 
+#     answer.append(str(arr.pop(num)))
+# print("<",", ".join(answer)[:],">", sep='')
