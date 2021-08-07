15d96e23df547ee6c62dec26bfd92a75749f0640
kimdoeon <ilsin205@naver.com>
2021-08-07 11:02:28 +0900

    Add bj_de

--- datastructure/스택/1918_de.py
+++ datastructure/스택/1918_de.py
@@ -6,7 +6,7 @@ operation = deque(list(sys.stdin.readline().rstrip()))
 opStack=[]
 
 #인덱스 연산을 할 땐 언제나 범위 에러 잡기
-
+#굳이 popleft를 안하고 바로 출력해도 되는 부분이었음. 출력하고 뒤로 돌아가지 않으니 popleft와 동일하 효과
 for i in range(len(operation)):
     if operation[0].isalpha():
         print(operation.popleft(), end="")
