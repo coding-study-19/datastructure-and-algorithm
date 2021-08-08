44b8481ef4f2327dac5def43259a30049269943b
kimdoeon <ilsin205@naver.com>
2021-08-08 16:28:12 +0900

    Add bj_de

--- datastructure/문자열/11655_de.py
+++ datastructure/문자열/11655_de.py
@@ -1,13 +1,11 @@
 #11655 ROT13
 
 import sys
-# 파이썬 문장 끝이 개행이어야 입력이 되었다는 뜻이인가 end=""이 문제가 아니라 첫 문장 받고 개행이 없어서
-# 계속 기다린거..? 그럼 지금까진 어케 됐지 이게
+
 while(1):
-    sentence = sys.stdin.readline().rstrip("\n")
+    sentence = sys.stdin.readline()
     if not sentence:
         break
-
     for each in sentence:
         if each.isupper():
             if ord(each)<78:
