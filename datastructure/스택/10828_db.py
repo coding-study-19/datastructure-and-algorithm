4a5479bd99a57dacac9a1566bb9272697284bbce
00kang <70834044+00kang@users.noreply.github.com>
2021-08-13 22:17:53 +0900

    Add bj_db

---
+++ datastructure/스택/10828_db.py
@@ -0,0 +1,24 @@
+# 백준 _ [200]자료구조1 _ #10828 _ 스택
+
+import sys
+input = sys.stdin.readline
+
+stack_list = []
+n = int(input())
+
+for x in range(n):
+    command = list(input().split())
+
+    if command[0] == 'push':
+        stack_list.append(command[1])
+    elif command[0] == 'pop':
+        if len(stack_list) == 0: print(-1)
+        else: print(stack_list.pop())
+    elif command[0] == 'size':
+        print(len(stack_list))
+    elif command[0] == 'empty':
+        if len(stack_list) == 0: print(1)
+        else: print(0)
+    elif command[0] == 'top':
+        if len(stack_list) == 0: print(-1)
+        else: print(stack_list[-1])
