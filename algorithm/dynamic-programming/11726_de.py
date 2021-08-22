97589c4d84e83d454f74c29e3e7d2f949ce419ec
kimdoeon <ilsin205@naver.com>
2021-08-22 23:39:32 +0900

    Add bj_de

--- algorithm/dynamic-programming/11726_de.py
+++ algorithm/dynamic-programming/11726_de.py
@@ -1,1 +1,34 @@
 # 11726 2xn 타일링
+#dp 점화식 : d[n] = d[n-1] + d[n-2]
+# 추가할 수 있는 직사각형의 가로 길이가 1과 2가 전부이므로 가로 길이가 n인 경우를
+# 구하려면 n-1길이의 경우에 1길이 직사각형 추가하는 경우 + n-2길이의 경우에 2길이 직사각형을 추가하는 경우
+n = int(input())
+memo = [None] * (n+1)
+def dp(n):
+    if n==1: 
+        return 1
+    elif n==2:
+        return 2
+    elif memo[n]:
+        return memo[n]
+    memo[n] = dp(n-1) + dp(n-2)
+    return memo[n]
+
+print(dp(n))
+    
+# 내코드 : dp 구현 못함
+# n = int(input())
+
+# # 팩토리얼 memozation
+# fac=[1,1]
+# for i in range(2,n+1):
+#     fac.append(i*fac[i-1])
+
+# def calc(n):
+#     result = 0
+#     arr =[i for i in range(n//2+1)]
+#     for num in arr:
+#         result+=fac[num+(n-num*2)]//(fac[num]*fac[n-num*2])
+#     print(result%10007)
+
+# calc(n)
--- algorithm/dynamic-programming/1463_de.py
+++ algorithm/dynamic-programming/1463_de.py
@@ -1,80 +1,73 @@
 # 1463 1로 만들기
 
-from collections import deque
+#  dp점화식 : d[N] = min(dp[N-1},dp[N//2],dp[n//3])+1
 N = int(input())
-arr=deque()
-arr.append(N)
-count = 0
 
-def calc(n):
-    # 2또는 3으로 나누어 떨어지는 경우
-    if n%3==0 and n%2==0:
-        arr.append(n//3)
-        arr.append(n//2)
-        arr.append(n-1)
-        return arr
+dp = [0] *(N+1)
 
-    # 3으로만 나누어 떨어지는 경우
-    elif n%3==0:
-        arr.append(n//3)
-        arr.append(n-1)
-        return arr
+for i in range(2,N+1):
 
-    #  2로만 나누어 떨어지는 경우
-    elif n%2==0:
-        arr.append(n//2)
-        arr.append(n-1)
-        return arr
+    #1을 뺐을 경우 연산 횟수 저장
+    dp[i] = dp[i-1]+1
 
-    #이외의 경우
-    else:
-        arr.append(n-1)
-        return arr
+    #i가 2로 나누어 떨어질 경우 i-1의 연산 횟수와 비교해서 최솟값 저장
+    if i%2==0:
+        dp[i] = min(dp[i], dp[i//2]+1)
 
-while 1:
-    if min(arr) == 1:
-        break
+    #i 가 3으로 나누어 떨어질 경우 위의 계산을 통해 얻은 최솟값과 i//3의 연산 횟수와 비교해 최솟값 저장
+    if i%3==0:
+        dp[i] = min(dp[i],dp[i//3]+1)
 
-    tmp = arr.copy()
-    for num in tmp:
-        calc(num)
-        arr.popleft()
+print(dp[-1])
+    
 
-    count+=1
-print(count)
-#------------------------------
 
+# 내 코드(memozation 구현 안됨)
+# from collections import deque
+# N = int(input())
+# arr=deque()
+# arr.append(N)
+# count = 0
 
 # def calc(n):
+#     # 2또는 3으로 나누어 떨어지는 경우
 #     if n%3==0 and n%2==0:
-#         return [n//3,n//2,n-1]
+#         arr.append(n//3)
+#         arr.append(n//2)
+#         arr.append(n-1)
+#         return arr
 
+#     # 3으로만 나누어 떨어지는 경우
 #     elif n%3==0:
-#         return [n//3,n-1]
+#         arr.append(n//3)
+#         arr.append(n-1)
+#         return arr
 
+#     #  2로만 나누어 떨어지는 경우
 #     elif n%2==0:
-#         return [n//2,n-1]
+#         arr.append(n//2)
+#         arr.append(n-1)
+#         return arr
 
+#     #이외의 경우
 #     else:
-#         return [n-1]
-
-# arr = calc(N)
-# tmp = arr.copy()
-# count = 0
+#         arr.append(n-1)
+#         return arr
 
 # while 1:
 #     if min(arr) == 1:
 #         break
 
+#     tmp = arr.copy()
 #     for num in tmp:
 #         calc(num)
 #         arr.popleft()
-#     count+=1
 
+#     count+=1
 # print(count)
 
-
-# 재귀는 재귀 횟수의 한계 때문에 실패
+# 내코드 2
+# 재귀 횟수의 한계 때문에 실패
 # def calc(n):
 #     if arr[n]:
 #         return arr[n]
