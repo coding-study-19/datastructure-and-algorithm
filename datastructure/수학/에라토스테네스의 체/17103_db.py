d6021fe7685767362a2a5c608607a4eb0716bb55
00kang <kdb070970@gmail.com>
2021-09-24 15:03:06 +0900

    commit dabin

---
+++ datastructure/수학/에라토스테네스의 체/17103_db.py
@@ -0,0 +1,27 @@
+# 백준 | [301]수학1 (연습) | #17103 | 골드바흐 파티션 | 수학
+
+def get_primary_list(n):
+    array = [1 for _ in range(n+1)]
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if array[i]:
+            j = 2
+
+        while i * j <= n:
+            array[i * j] = 0
+            j += 1
+
+    return array
+
+T = int(input())
+nums = [int(input()) for _ in range(T)]
+max_num = max(nums)
+primarys = get_primary_list(max_num)
+
+for num in nums:
+    result = 0
+    for i in range(2, num // 2 + 1):
+        if primarys[i] and primarys[num - i]:
+            result += 1
+
+    print(result)
